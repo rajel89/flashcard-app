86dd9bafa97f9f88e2a0bde0350f56e82c6fdfe9
rajel89 <rajah.ello@yahoo.com>
2021-08-15 10:00:54 -0500

    installed react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import FlashcardList from "./FlashcardList";
+import "./app.css";
+import axios from "axios";
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(() => {
+    axios
+      // .get("https://opentdb.com/api.php?amount=10")
+      // .get('mongodb+srv://new-user-01:YSx371pVxKIbHNnb@cluster0.2jmzi.mongodb.net/deck_of_cards?retryWrites=true&w=majority')
+      .get("http://localhost:6000/api/collections")
+      .then((res) => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer);
+            const options = [
+              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+              answer,
+            ];
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - 0.5),
+            };
+          })
+        );
+      });
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textArea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <FlashcardList flashcards={flashcards} />
     </div>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    // id: 1,
+    question: "What is 2+2?",
+    answer: "4",
+    // options:[
+    //     '2',
+    //     '3',
+    //     '4',
+    //     '5'
+    // ]
+  },
+  {
+    // id: 2,
+    question: "Question 2?",
+    answer: "Answer",
+    // options: [
+    //     'Answer',
+    //     'Answer 1',
+    //     'Answer 2',
+    //     'Answer 3'
+    // ]
+  },
+];
 
 export default App;
---
+++ src/FlashcardList.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Flashcard from "./Flashcard";
+
+
+export default function FlashcardList({ flashcards }) {
+  return (
+    <div claseName="card-grid">
+      {flashcards.map(flashcard => {
+        return <Flashcard flashcard={flashcard} key={flashcard.id} />
+      })}
+    
+      </div>
+  );
+}
+
+
+
